d1770b682e060819baef0f9ded6869f170a70c4e
weiyuan <weiyuan@fiiser.com>
2018-11-20 15:41:46 +0800

    implement Read view: /todos and /todos/:id

--- app.js
+++ app.js
@@ -26,7 +26,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/todos', (req, res) => {
-  return res.send('get /todos')
+  Todo.find((err, todos) => {
+    if (err) return console.error(err)
+    return res.render('todos', {todos: todos})
+  })
 })
 
 app.get('/todos/new', (req, res) => {
@@ -38,7 +41,10 @@ app.get('/todos/edit', (req, res) => {
 })
 
 app.get('/todos/:id', (req, res) => {
-  return res.send('get /todos/:id')
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.error(err)
+    return res.render('todo', {todo: todo})
+  })
 })
 
 app.post('/todos', (req, res) => {
