5f48525b248e7ecd55e03c486ef96d5b456ff493
weiyuan <weiyuan@fiiser.com>
2018-11-20 15:22:27 +0800

    mongoose init

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const port = 3000
+
+const mongoURI = 'mongodb://127.0.0.1/todo-mongoose'
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', () => console.log(`Connected to MongoDB`))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
