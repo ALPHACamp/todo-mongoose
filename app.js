2487391b944f6d9ce20b2a0e23e8b8cb8f38276b
weiyuan <weiyuan@fiiser.com>
2018-11-20 16:14:05 +0800

    attach methodOverride to implement RESTFul route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
 const bodyParser= require('body-parser')
 const app = express()
 const port = 3000
@@ -8,6 +9,7 @@ const port = 3000
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 const mongoURI = 'mongodb://127.0.0.1/todo-mongoose'
 mongoose.connect(mongoURI, { useNewUrlParser: true })
@@ -66,7 +68,7 @@ app.post('/todos', (req, res) => {
   })
 })
 
-app.post('/todos/:id', (req, res) => {
+app.put('/todos/:id', (req, res) => {
   Todo.findById(req.params.id, (err, todo) => {
     if (err) return console.error(err)
     todo.name = req.body.name,
@@ -78,7 +80,7 @@ app.post('/todos/:id', (req, res) => {
   })
 })
 
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id/delete', (req, res) => {
   Todo.findById(req.params.id, (err, todo) => {
     if (err) return console.error(err)
     todo.remove((err) =>{
